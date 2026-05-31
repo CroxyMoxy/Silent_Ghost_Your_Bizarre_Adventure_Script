70e38fddd80e30feed09a9ed6bfb7bfb2b1c6386
CroxyMoxy <SercolSamos@proton.me>
2026-05-31 19:00:21 +0200

    Update AutoUpdate.lua

--- AutoUpdate.lua
+++ AutoUpdate.lua
@@ -1,1 +1,1 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CroxyMoxy/Silent_Ghost_Your_Bizarre_Adventure_Script/refs/heads/main/Version_0_1.lua?token=GHSAT0AAAAAAD6QU62Q2PMVS7PSBKQBKNUQ2Q4NFLA"))()
